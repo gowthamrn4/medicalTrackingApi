a5338df9921edf9051542bb8394d18f40722202f
gowthamrn4 <gowthamrn4@gmail.com>
2019-03-09 14:29:53 +0530

    first commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-    'database': 'mongodb://admin123:admin123@ds349175.mlab.com:49175/medicaltacking'    
+    'database': 'mongodb://gowtham:admin123@ds349175.mlab.com:49175/medicaltacking'    
     // 'database': 'mongodb://admin:admin12345@ds153593.mlab.com:53593/courierssystem'    
 
 };
